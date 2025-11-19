7d68d1efad70f1c83d4c17e54ceb6beb19182730
Mudwin <nshmilsson@gmail.com>
2025-11-19 23:02:20 +0500

    feat: create routes in App component to admin panel pages

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,3 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Administrators from './pages/Administrators/Administrators';
+import Clients from './pages/Clients/Clients';
+import Constructor from './pages/Constructor/Constructor';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Orders from './pages/Orders/Orders';
+import Products from './pages/Products/Products';
+import Settings from './pages/Settings/Settings';
+
 export default function App() {
-  return <div>hi</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/clients" element={<Clients />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/administrators" element={<Administrators />} />
+        <Route path="/constructor" element={<Constructor />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ admin/src/pages/Administrators/Administrators.tsx
@@ -0,0 +1,3 @@
+export default function Administrators() {
+  return <div>admins</div>;
+}
