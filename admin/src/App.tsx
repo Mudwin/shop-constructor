f93d73ea0796593d1ff89845d9e043e35590971f
Mudwin <nshmilsson@gmail.com>
2025-11-20 12:21:56 +0500

    feat: create Navbar component

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
 import Administrators from './pages/Administrators/Administrators';
 import Clients from './pages/Clients/Clients';
 import Constructor from './pages/Constructor/Constructor';
@@ -11,13 +12,15 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/clients" element={<Clients />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/administrators" element={<Administrators />} />
-        <Route path="/constructor" element={<Constructor />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/clients" element={<Clients />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/administrators" element={<Administrators />} />
+          <Route path="/constructor" element={<Constructor />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ admin/src/types/index.ts
@@ -0,0 +1,8 @@
+export type AdminPage =
+  | 'dashboard'
+  | 'orders'
+  | 'products'
+  | 'clients'
+  | 'settings'
+  | 'administrators'
+  | 'constructor';
